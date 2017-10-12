13a1122da622b97a144c98a6f75aecfe946765b2
Daniele Canavese <daniele.canavese@polito.it>
2017-10-12 18:32:36 +0200

    Changed the version number to 0.5

--- cybertop/util.py
+++ cybertop/util.py
@@ -47,7 +47,7 @@ PID_FILE = "/tmp/cybertop.pid"
 # The configuration file.
 CONFIGURATION_FILE="/etc/cybertop.cfg"
 # The version.
-VERSION="0.2"
+VERSION="0.5"
 
 def getPluginDirectory():
     """
