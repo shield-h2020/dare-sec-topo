81a4601c09a9dd4a177896e7b887ba21cc892b30
Marco De Benedictis <marco.debenedictis@polito.it>
2018-10-19 11:30:13 +0100

    Updated worm parser
    - Exchanged parser of source with destination ip address

--- cybertop/plugins/ParserWorm.py
+++ cybertop/plugins/ParserWorm.py
@@ -1,11 +1,11 @@
 # Copyright 2017 Politecnico di Torino
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,7 +29,7 @@ class ParserWorm(ParserPlugin):
     """
     Parses a Worm attack event.
     """
-    
+
     def parse(self, fileName, count, line):
         """
         Parses an event line.
@@ -39,19 +39,19 @@ class ParserWorm(ParserPlugin):
         @return: The attack event or None if this line should be silently ignored.
         @raise IOError: if the line contains something invalid.
         """
-        
+
         if re.match("\s*#.*", line):
             return None
-        
+
         parts = re.split("\s*,\s*|\s+", line.rstrip())
-                
+
         if parts == [""]:
             return None
-        
+
         try:
             timestamp = parser.parse("%s %s" % (parts[0], parts[1]))
-            destinationAddress = ipaddress.ip_address(parts[9])
-            sourceAddress = ipaddress.ip_address(parts[10])
+            sourceAddress = ipaddress.ip_address(parts[9])
+            destinationAddress = ipaddress.ip_address(parts[10])
             destinationPort = int(parts[11])
             sourcePort = int(parts[12])
             protocol = parts[13]
@@ -59,7 +59,7 @@ class ParserWorm(ParserPlugin):
             inputBytes = int(parts[15])
             outputPackets = int(parts[16])
             outputBytes = int(parts[17])
-            
+
             attackEvent = AttackEvent(timestamp, "%s:%d" % (sourceAddress, sourcePort), "%s:%d" % (destinationAddress, destinationPort))
             attackEvent.fields["protocol"] = protocol
             attackEvent.fields["inputPackets"] = inputPackets
